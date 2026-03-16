8c2fbdc0ca364775a70974ca52c09dcef4532574
libokai <kevin2005ha@gmail.com>
2026-03-16 23:07:46 +0800

    Initial Commit

---
+++ CodePulseTests/CodePulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodePulseTests.swift
+//  CodePulseTests
+//
+//  Created by 李博凱 on 2026/3/16.
+//
+
+import Testing
+@testable import CodePulse
+
+struct CodePulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

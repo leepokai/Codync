ae68a92a2d4e4135c621f4486c20c08691324293
libokai <kevin2005ha@gmail.com>
2026-03-16 23:07:46 +0800

    chore: initial commit

---
+++ CodePulse/CodePulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodePulseApp.swift
+//  CodePulse
+//
+//  Created by 李博凱 on 2026/3/16.
+//
+
+import SwiftUI
+
+@main
+struct CodePulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
